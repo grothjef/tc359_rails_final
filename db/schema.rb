dd010ae1a28e69003bf17ff4f8b6bc3545f0fa2b
Jeff Groth <grothjef@gmail.com>
2015-04-05 14:47:38 -0400

    Authentication
    '

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323155941) do
+ActiveRecord::Schema.define(version: 20150401001024) do
 
   create_table "heros", force: :cascade do |t|
     t.text     "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150323155941) do
     t.boolean  "intelligence"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
